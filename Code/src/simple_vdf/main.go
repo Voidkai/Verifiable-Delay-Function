4484ac8d7b1df2e5383fe0afd4c09fcd8039c2cd
kaixuan <825839697@qq.com>
2020-12-16 22:38:08 +0800

    “”

---
+++ Code/src/simple_vdf/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+)
+
+func main(){
+	var security uint64 = 300
+	n := 20
+	T := uint64(1 << 20)
+	N := new(big.Int).Set(Setup(security))
+
+	fmt.Println("security:",security,"T: ",T,"= 2^",n, "N:",N)
+
+	instance := Generate(N, T, security)
+
+	Time_Start := time.Now()
+	instance.NaiveSolve()
+
+	fmt.Println("time:", time.Since(Time_Start))
+	fmt.Println(instance.challenge , instance.y)
+
+}
